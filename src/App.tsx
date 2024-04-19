d88dfcfdac5729ea9c5d7082968d9bd7f285330a
Emmanuel Vargas-Zapata <emmanuel.vargas@lc.cuny.edu>
2024-04-19 16:19:46 -0400

    imported routes to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages';
 
 const App = () => {
     return (
@@ -7,6 +10,8 @@ const App = () => {
             <Routes>
                 {/* public routes */}
                 <Route path="/sign-in" element={<SigninForm />} />
+                <Route path="/sign-up" element={<SignupForm />} />
+
                 {/* private routes */}
                 <Route index element = {<Home />}/>
             </Routes>
