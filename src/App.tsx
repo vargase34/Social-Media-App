c9665061a3b0ae388cb72d8cade141b9114a0c44
Emmanuel Vargas-Zapata <emmanuel.vargas@lc.cuny.edu>
2024-04-19 15:54:16 -0400

    added public and private routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
 import './globals.css';
 
 const App = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                My Digital Diary
-            </h1>
-        </div>
+        <main className="flex h-screen">
+            <Routes>
+                {/* public routes */}
+                <Route path="/sign-in" element={<SigninForm />} />
+                {/* private routes */}
+                <Route index element = {<Home />}/>
+            </Routes>
+            <div>
+                <h1 className="text-3xl font-bold underline">
+                    My Digital Diary
+                </h1>
+            </div>
+        </main>
     )
 }
 
